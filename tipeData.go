97bc65760a8a627846a8a594d9ec5b8cb0affa3b
ars313 <kholis13@gmail.com>
2020-12-05 20:17:19 +0700

    add tipeData.go update

--- tipeData.go
+++ tipeData.go
@@ -6,8 +6,11 @@ func main() {
 	var positiveNumber uint8 = 89
 	var negativeNumber = -12334334334
 	var decimalNumber = 2.62
+	var exist bool = true
+
 	fmt.Printf("Bilangan positif: %d\n", positiveNumber)
 	fmt.Printf("bilangan negatif: %d\n", negativeNumber)
 	fmt.Printf("Deicimal number: %f\n", decimalNumber)
 	fmt.Printf("Decimal number%.3f\n", decimalNumber)
+	fmt.Printf("exist? %t\n", exist)
 }
