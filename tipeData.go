6d0760f5bdd379dd4dc44859ea90ec1f344efeee
ars313 <kholis13@gmail.com>
2020-12-05 20:12:28 +0700

    add tipeData.go

---
+++ tipeData.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	var positiveNumber uint8 = 89
+	var negativeNumber = -12334334334
+	var decimalNumber = 2.62
+	fmt.Printf("Bilangan positif: %d\n", positiveNumber)
+	fmt.Printf("bilangan negatif: %d\n", negativeNumber)
+	fmt.Printf("Deicimal number: %f\n", decimalNumber)
+	fmt.Printf("Decimal number%.3f\n", decimalNumber)
+}
